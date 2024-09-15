92396a6a38a65efb8b2ba2424e2e7d3aa7913c87
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-09-15 16:52:52 -0300

    feat: Criada classe 'EmailValidator', para máxima performance utilizando 'readonly' em tipo 'SearchValues<T>'

---
+++ SearchValues/EmailValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Buffers;
+
+namespace SearchValuesNamespace;
+internal static class EmailValidator
+{
+    private static readonly string _allowedCharsEmailString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@._-";
+    private static readonly SearchValues<char> _allowedCharsEmailSearchValues = SearchValues.Create(_allowedCharsEmailString);
+
+    internal static bool IsValidEmail(string stringToBeAnalyzed)
+    {
+        return stringToBeAnalyzed
+            .AsSpan()
+            .IndexOfAnyExcept(_allowedCharsEmailSearchValues) == -1;
+    }
+}
