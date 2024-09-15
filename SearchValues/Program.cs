567b10b0954e90bf2e2cd28d515d140311526eb6
Lucas de Oliveira Gonçalves <lucas.oliver_2000@hotmail.com>
2024-09-15 16:45:44 -0300

    Add project files.

---
+++ SearchValues/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Buffers;
+
+string stringToBeAnalyzed = "example@email.com";
+Console.WriteLine(IsValidEmail(stringToBeAnalyzed));
+
+stringToBeAnalyzed = "!nvalid#@ email.com";
+Console.WriteLine(IsValidEmail(stringToBeAnalyzed));
+
+static bool IsValidEmail(string stringToBeAnalyzed)
+{
+    string allowedCharsEmailString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@._-";
+    SearchValues<char> allowedCharsEmailSearchValues = SearchValues.Create(allowedCharsEmailString);
+
+    return stringToBeAnalyzed
+        .AsSpan()
+        .IndexOfAnyExcept(allowedCharsEmailSearchValues) == -1;
+}
